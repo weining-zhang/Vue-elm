05590f2179e0ea647a21480b420814a95428e8d1
welnee <445103710@qq.com>
2020-12-15 00:46:32 +0800

    :pencil2: feat: update

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="city_container">
-    <HeadTop>
+
+    <HeadTop :headTitle="cityName" :goBack="true">
       <router-link to="/home" class="change_city">
-        <template #changecity> <div>切换城市</div> </template>
+        <template #changecity><span>切换城市</span></template>
       </router-link>
     </HeadTop>
   </div>
@@ -10,15 +11,38 @@
 
 <script>
   import HeadTop from 'components/header/Head.vue'
+  import { currentCity } from 'network/getData'
 
   export default {
     name: "City",
     components:{
       HeadTop
     },
+    data() {
+      return {
+        cityId: '',   // 当前城市id
+        cityName: ''  // 当前城市名字
+      }
+    },
+    mounted() {
+      this.cityId = this.$route.params.cityId
+      currentCity(this.cityId).then(res => {
+        this.cityName = res.name
+      })
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  @import 'assets/scss/mixin';
 
+  .city_container {
+    padding-top: 54.99px;
+  }
+
+  .change_city {
+    @include sc(14.04px, #fff);
+    @include ct;
+    right: 9.36px;
+  }
 </style>
