559e2c8e194727ec057fcb05f0ea0cc9e12a00a6
welnee <445103710@qq.com>
2021-04-06 00:09:37 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,8 @@ import actions from './actions'
 Vue.use(Vuex)
 
 const state = {
-  latitude: '',                  // 当前位置纬度
-	longitude: '',                 // 当前位置经度
+  // latitude: '',                  // 当前位置纬度
+	// longitude: '',                 // 当前位置经度
   userInfo: null,                // 用户信息
   login: true,                   // 是否登录
   geohash: '31.22299,121.36025', // 地址 geohash 值
--- src/views/food/Food.vue
+++ src/views/food/Food.vue
@@ -4,8 +4,8 @@
 
     <section class="sort_container">
       <!-- 分类 -->
-      <div class="sort_item" :class="{}">
-        <div class="sort_item_container">
+      <div class="sort_item" :class="{choose_type:sortBy === 'food'}">
+        <div class="sort_item_container" @click="chooseType('food')">
           <div class="sort_item_border">
             <span>{{foodTitle}}</span>
             <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" version="1.1" class="sort_icon">
@@ -15,9 +15,30 @@
         </div>
 
         <transition name="showlist" v-show="category">
+          <section v-show="sortBy === 'food'" class="category_container sort_detail_type">
+            <section class="category_left">
 
+            </section>
+          </section>
         </transition>
       </div>
+
+			<!-- 排序 -->
+      <div class="sort_item" :class="{choose_type:sortBy === 'sort'}">
+        <div class="sort_item_container" @click="chooseType('sort')">
+          <div class="sort_item_border">
+            <span :class="{category_title: sortBy == 'sort'}">排序</span>
+            <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" version="1.1" class="sort_icon">
+              <polygon points="0,3 10,3 5,8"/>
+            </svg>
+          </div>
+        </div>
+
+        <transition name="showlist">
+          
+        </transition>
+      </div>
+      
 
     </section>
   </div>
@@ -40,6 +61,7 @@
         geohash: '',    // city页面传递过来的地址geohash
         headTitle: '',  // msiet页面头部标题
         foodTitle: '',  // 排序左侧头部标题
+        sortBy: '',     // 筛选的条件
         category: null, // category分类左侧数据
       }
     },
@@ -57,6 +79,12 @@
 
         // 获取 category 分类左侧数据
         this.category = await foodCategory(this.latitude, this.longitude);
+      },
+
+
+      // 点击顶部三个选项，展示不同的列表，选中当前选项进行展示，同时收回其他选项
+      choose_type(type) {
+
       }
     }
   }
