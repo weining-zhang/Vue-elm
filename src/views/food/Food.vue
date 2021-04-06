e338d461a9eaaabb075766c5527e0bab7a0f1020
weining-zhang <445103710@qq.com>
2021-04-06 22:03:41 +0800

    update

--- src/views/food/Food.vue
+++ src/views/food/Food.vue
@@ -39,7 +39,36 @@
         </transition>
       </div>
       
+			<!-- 筛选 -->
+      <div class="sort_item" :class="{choose_type:sortBy === 'activity'}">
+        <div class="sort_item_container" @click="chooseType('activity')">
+          <span :class="{category_title: sortBy == 'activity'}">筛选</span>
+          <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" version="1.1" class="sort_icon">
+            <polygon points="0,3 10,3 5,8"/>
+          </svg>
+        </div>
 
+        <transition name="showlist">
+
+        </transition>
+      </div>
+      
+      <transition name="showcover">
+        <div class="back_cover" v-show="sortBy"></div>
+      </transition>
+
+      <section class="shop_list_container">
+        <Shoplist 
+          :geohash="geohash" 
+          :restaurantCategoryId="restaurant_category_id"
+          :restaurantCategoryIds="restaurant_category_ids"
+          :sortByType="sortByType"
+          :deliveryMode="delivery_mode" 
+          :confirmSelect="confirmStatus" 
+          :supportIds="support_ids"
+          v-if="latitude"
+        />
+      </section>
     </section>
   </div>
 </template>
@@ -47,6 +76,7 @@
 <script>
   import { mapState, mapMutations } from "vuex";
   import NavBar from 'components/header/Head.vue'
+  import Shoplist from 'components/content/shops/Shoplist.vue';
   import {
     foodCategory
   } from 'network/getData';
@@ -54,15 +84,24 @@
   export default {
     name: "Food",
     components:{
-      NavBar
+      NavBar,
+      Shoplist
     },
     data() {
       return {
-        geohash: '',    // city页面传递过来的地址geohash
-        headTitle: '',  // msiet页面头部标题
-        foodTitle: '',  // 排序左侧头部标题
-        sortBy: '',     // 筛选的条件
-        category: null, // category分类左侧数据
+        geohash: '',                 // city页面传递过来的地址geohash
+        headTitle: '',               // msiet页面头部标题
+        foodTitle: '',               // 排序左侧头部标题
+        restaurant_category_id: '',  // 食品类型id值
+        restaurant_category_ids: '', // 筛选类型的id
+        sortBy: '',                  // 筛选的条件
+        category: null,              // category分类左侧数据
+        categoryDetail: null,        // category分类右侧的详细数据
+        sortByType: null,            // 根据何种方式排序
+        Delivery: null,              // 配送方式数据
+        support_ids: [],             // 选中的商铺活动列表
+        delivery_mode: null,         // 选中的配送方式
+        confirmStatus: false         // 确认选择
       }
     },
     created() {
@@ -74,8 +113,11 @@
     methods: {
       // 初始化获取数据
       async initData() {
+        // 获取从msite页面传递过来的参数
+        this.geohash = this.$route.query.geohash;
         this.headTitle = this.$route.query.title;
         this.foodTitle = this.headTitle;
+        this.restaurant_category_id = this.$$route.query.restaurant_category_id;
 
         // 获取 category 分类左侧数据
         this.category = await foodCategory(this.latitude, this.longitude);
@@ -85,6 +127,19 @@
       // 点击顶部三个选项，展示不同的列表，选中当前选项进行展示，同时收回其他选项
       choose_type(type) {
 
+      },
+
+      // 选中Category左侧列表的某个选项时，右侧渲染相应的sub_categories列表
+      selectCategoryName(id, index) {
+          // 第一个选项 -- 全部商家 因为没有自己的列表，所以点击则默认获取选所有数据
+        if (index === 0) {
+          this.restaurant_category_ids = null;
+          this.sortBy = '';
+        } else {
+          //不是第一个选项时，右侧展示其子级sub_categories的列表
+          this.restaurant_category_ids = id;
+          this.categoryDetail = this.category[index].sub_categories;
+        }
       }
     }
   }
@@ -94,7 +149,7 @@
   @import 'assets/scss/mixin';
 
   .food_container {
-    padding-top: 3.6rem;
+    padding-top: 7px;
   }
 
   .sort_container {
@@ -118,7 +173,7 @@
         z-index: 14;
         background-color: #fff;
         box-sizing: border-box;
-        padding-top: 0.3rem;
+        padding-top: 7px;
         .sort_item_border {
           height: 1rem;
           border-right: 0.025rem solid $bc;
